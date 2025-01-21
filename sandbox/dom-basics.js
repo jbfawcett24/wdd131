b3d0d474b84331e498fe056b3d2d4c241c7950a3
jbfawcett24 <jbfawcett24@gmail.com>
2025-01-21 10:18:33 -0700

    some prepare stuff done

--- sandbox/dom-basics.js
+++ sandbox/dom-basics.js
@@ -11,3 +11,16 @@ document.body.appendChild(newParagraph);
 const newSection = document.createElement("section");
 newSection.innerHTML = "<h2>Dom Basics</h2> <p>This was added through Javascript";
 document.body.appendChild(newSection);
+
+const ingredientList = ["Pinto Beans", "Corn", "Spices", "Tortillas"];
+const portionData = ["1 15oz can", "1 15 oz can", "1 Tbsp", "8"];
+
+const recipe = document.createElement("ul");
+recipe.innerHTML = "<em>Ingredient List</em>";
+for(let i = 0; i < ingredientList.length; i++)
+{
+    const newListEntry = document.createElement("li");
+    newListEntry.innerText = `${portionData[i]} - ${ingredientList[i]}`;
+    recipe.appendChild(newListEntry);
+}
+document.body.append(recipe);
